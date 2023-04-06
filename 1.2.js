670577968f6f7bfbe908a496fba01f6abffb215e
Benja <benjaminmayeur01@gmail.com>
2023-04-06 11:26:22 +0200

    exo 1.2

---
+++ 1.2.js
@@ -0,0 +1,7 @@
+
+const name1 = {}; //car name a déjà été utilisé dans l'exercice 1.1.js ?
+const firstName = {};
+name1.name = "Lambert";
+firstName.name = "Gerard";
+let city = "Paris";
+console.log("Your name is " + name1.name + " " + firstName.name + " and you live in " + city);
