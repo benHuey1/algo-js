1daefb91af378f52696a1f6663b3b746f389bc07
Benja <benjaminmayeur01@gmail.com>
2023-04-07 09:23:05 +0200

    exo 2.6 terminé

--- 2.6.js
+++ 2.6.js
@@ -1,27 +1,75 @@
-let number = prompt("Please enter a number between 1 & 7 includes", "");
+let number = new Number(prompt("Please enter a number between 1 & 7 includes")).valueOf();
 let day = "";
+
 switch (number) {
-    case 1:
+    case 1: {
         day = "Monday";
-        console.log('${day}');
         break;
-    case 2:
+    }
+    case 2: {
         day = "Tuesday";
         break;
-    case 3:
+    }
+    case 3: {
         day = "Wednesday";
         break;
-    case 4:
+    }
+    case 4: {
         day = "Thursday";
         break;
-    case 5:
+    }
+    case 5: {
         day = "Friday";
         break;
-    case 6:
+    }
+    case 6: {
         day = "Saturday";
         break;
-    case 7:
+    }
+    case 7: {
         day = "Sunday";
         break;
+    }
+    default: {
+        day = "NOTHING";
+        alert("This is not a number between 1 & 7 includes");
+    }
 }
-alert(day + 'ok');
+alert("So it's  " + day + " today !");
+
+/*
+switch (number) {
+    case 1: {
+        alert("So it's Monday today !");
+        break;
+    }
+    case 2: {
+        alert("So it's Tuesday today !");
+        break;
+    }
+    case 3: {
+        alert("So it's Wednesday today !");
+        break;
+    }
+    case 4: {
+        alert("So it's Thursday today !");
+        break;
+    }
+    case 5: {
+        alert("So it's Friday today !");
+        break;
+    }
+    case 6: {
+        alert("So it's Saturday today !");
+
+        break;
+    }
+    case 7: {
+        alert("So it's Sunday today !");
+        break;
+    }
+    default: {
+        alert("This is not a number between 1 & 7 includes");
+    }
+}
+*/
