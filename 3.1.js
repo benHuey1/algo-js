1f60e3bdfe7391bdeece027a7fc32911bd827ca9
Benja <benjaminmayeur01@gmail.com>
2023-04-07 11:45:17 +0200

    exo 3.1.js fini

---
+++ 3.1.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 3, 4, 5]
+total = 0;
+for (var i in arr) {
+    total += arr[i];
+}
+alert("The sum of the numbers in my array is : " + total);
+
+let arr_2 = [100, 101, 102]
+total_2 = 0;
+for (var i in arr_2) {
+    total_2 += arr_2[i];
+}
+alert("The sum of the numbers in my second array is : " + total_2);
