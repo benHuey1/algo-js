98df467bd6d0bcfb6a0c259d036a186880a415f7
Benja <benjaminmayeur01@gmail.com>
2023-04-07 11:38:49 +0200

    exo 3.0.js fini

---
+++ 3.0.js
@@ -0,0 +1,4 @@
+let arr = ["Abel", "Alexandra", "Anthony", "Benjamin", "Bruno", "Delphine", "Emilien", "Ethan", "Islmaël", "Jodie", "Julien", "Louka", "Loïc", "Luciano", "Nikko", "Steve", "Sylvain", "Thomas", "Willy"];
+for (let elem of arr) {
+    console.log("Do you kwow " + elem + "?");
+}
