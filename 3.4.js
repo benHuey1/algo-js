e4f6a51c9219cf23a28ced582a1a3a98aa014634
Benja <benjaminmayeur01@gmail.com>
2023-04-07 13:59:58 +0200

    exo 3.4.js fini

---
+++ 3.4.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3, 4, 5];
+var min = Math.min(...arr);
+console.log("The minimum value of the array is " + min);
+var max = Math.max(...arr);
+console.log("The maximum value of the array is " + max);
